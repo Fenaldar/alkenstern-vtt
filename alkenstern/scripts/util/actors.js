e4ae3b3b69abc548c5d6a30bedd6493c0c021d5f
Fenaldar <49685258+Fenaldar@users.noreply.github.com>
2026-02-26 13:46:28 +0100

    Add functions to get selected and scene characters

--- alkenstern/scripts/util/actors.js
+++ alkenstern/scripts/util/actors.js
@@ -1,1 +1,17 @@
 // getSelectedCharacters(), getSceneCharacters()
+export function getSelectedCharacters() {
+  return (canvas.tokens.controlled ?? [])
+    .map(t => t.actor)
+    .filter(a => a?.type === "character");
+}
+
+export function getSceneCharacters({ unique = true, visibleOnly = false } = {}) {
+  const tokens = canvas.tokens.placeables
+    .filter(t => (visibleOnly ? t.visible : true))
+    .filter(t => t.actor?.type === "character");
+
+  const actors = tokens.map(t => t.actor);
+
+  if (!unique) return actors;
+  return [...new Map(actors.map(a => [a.id, a])).values()];
+}
